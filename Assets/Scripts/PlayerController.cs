14d358056af97f35009ff4cdc737e826d810b797
Elias Segundo Antonio <eliassegundo.segundo@gmail.com>
2021-09-17 21:40:11 -0500

    Refactor: Player controller is now a Agent Class, Observations and actions are synced

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Agent
 {
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Vector2 newVelocity;
     private Animator anim;
-    private BoxCollider2D bc;
+    private CapsuleCollider2D cc;
+
+
 
     [Header("Movement params")]
     [SerializeField]
@@ -20,48 +25,88 @@ public class PlayerController : MonoBehaviour
     [Range(1, 10)]
     private int movementForce;
 
+    [SerializeField]
+    [Range(1, 5)]
+    private int jumpForce;
+
+    [Header("Target component")]
+    [SerializeField]
+    private Transform targetTransform;
+    
+
 
     // Start is called before the first frame update
-    void Start()
+    public override void Initialize()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        bc = GetComponent<BoxCollider2D>();
+        cc = GetComponent<CapsuleCollider2D>();
     }
 
-    public void moveRight(InputAction.CallbackContext context )
+    public override void OnEpisodeBegin()
     {
-        if(context.performed)
+        transform.localPosition = new Vector3(Random.Range(-8, 8), transform.localPosition.y, transform.localPosition.z);
+        targetTransform.localPosition= new Vector3(Random.Range(-8, 8), transform.localPosition.y, transform.localPosition.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        int movementAction = actions.DiscreteActions[0];
+        Debug.Log(movementAction);
+        switch(movementAction)
         {
-            Debug.Log("Move Right");
+            case 0:
+                moveRight();
+                break;
+            case 1:
+                moveLeft();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        if(Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            moveRight();
+        }
+        else if(Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            moveLeft();
+        }
+    }
+
+    public void moveRight()
+    {
             anim.SetBool("isWalking", true);
             newVelocity.Set(movementForce * speed, rb.velocity.y);
             rb.velocity = newVelocity;
+
+
             sr.flipX = false;
-            bc.offset = new Vector2(Mathf.Abs(bc.offset.x), bc.offset.y);
-        }
-        else if (context.canceled)
-        {
-            cancelMovement();
-        }
-       
+            cc.offset = new Vector2(0.63f, cc.offset.y); 
     }
 
-    public void moveLeft(InputAction.CallbackContext context)
+    public void moveLeft()
     {
-        if (context.performed)
-        {
-            anim.SetBool("isWalking", true);
-            newVelocity.Set(-movementForce * speed, rb.velocity.y);
-            rb.velocity = newVelocity;
-            sr.flipX = true;
-            bc.offset = new Vector2(-bc.offset.x, bc.offset.y);
-        }
-        else if (context.canceled)
-        {
-            cancelMovement();
-        }
+        anim.SetBool("isWalking", true);
+        newVelocity.Set(-movementForce * speed, rb.velocity.y);
+        rb.velocity = newVelocity;
+            
+        sr.flipX = true;
+        cc.offset = new Vector2(-0.63f, cc.offset.y);
+     
+    }
+
+    public void jump(InputAction.CallbackContext context)
+    {
+            rb.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
+            Debug.Log("Jump");
     }
 
     void cancelMovement()
@@ -69,11 +114,35 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isWalking", false);
         newVelocity.Set(0, rb.velocity.y);
         rb.velocity = newVelocity;
+
+        float newOffset = 0.3f;
+        if (sr.flipX == true) newOffset *= -1;
+        cc.offset = new Vector2(newOffset, cc.offset.y);
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        //This reward system is simply based on getting the coin or getting out of bounds
+        if(collision.CompareTag("Coin"))
+        {
+            AddReward(+1f);
+            EndEpisode();
+        }
+        else if (collision.CompareTag("Bound"))
+        {
+            AddReward(-1f);
+            EndEpisode();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
     }
+
 }
